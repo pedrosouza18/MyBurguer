0c15cd282f8a81689cb47e4670e4f3906a626814
Pedro Souza <souzapedro70@gmail.com>
2020-04-13 22:26:42 -0300

    fix: 🐛 Fix

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import React, { Component } from 'react';
+
+import Layout from './components/Layout/Layout';
+import BurguerBuilder from './containers/BurguerBuilder/BurguerBuilder';
 import './App.css';
 
 class App extends Component {
   render() {
     return (
       <div>
-        
+        <Layout>
+          <BurguerBuilder />
+        </Layout>
       </div>
     );
   }
