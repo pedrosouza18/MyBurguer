74d9c4b2b48bd25277650f5d5d8662cfe226a1de
Pedro Souza <souzapedro70@gmail.com>
2020-09-03 15:11:07 -0300

    let navigation component responsive

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -2,10 +2,12 @@ import React from 'react'
 import layoutClass from './Layout.module.css'
 
 import Toolbar from '../Navigation/Toolbar/Toolbar'
+import SideDrawer from '../Navigation/SideDrawer/SideDrawer'
 
 const layout = props => (
   <>
     <Toolbar />
+    <SideDrawer />
     <main className={layoutClass.Content}>{props.children}</main>
   </>
 )
